05481f3383bc57893e43312e7a9993d5c8984762
Philipp Gast <gast.philipp@gmail.com>
2024-02-17 17:11:11 +0100

    gradle init with micronaut openapi

---
+++ src/main/java/com/github/aloutta/publishers/Application.java
@@ -0,0 +1,11 @@
+package com.github.aloutta.publishers;
+
+import io.micronaut.runtime.*;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+
+}
