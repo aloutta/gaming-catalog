55c970f642ef06a32a2ed7ebbf9ed8af6770305f
Philipp Gast <gast.philipp@gmail.com>
2024-02-29 00:01:49 +0100

    removed Community domain, will be implemented in other service

--- src/main/java/com/github/aloutta/gaming/catalog/data/CommunityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.aloutta.gaming.catalog.data;
-
-import io.micronaut.core.annotation.*;
-import io.micronaut.data.annotation.*;
-import io.micronaut.data.repository.reactive.*;
-import reactor.core.publisher.*;
-
-@Repository
-public interface CommunityRepository extends ReactorPageableRepository<Community, Long> {}
